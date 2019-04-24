68c4494c750f93f287734bff5990daa90bf5f702
Perry Brandiezs <pbrandiezs@gmail.com>
2019-04-24 09:00:51 -0700

    feat: Updated views.py with new example code

--- views.py
+++ views.py
@@ -1,12 +1,12 @@
-#!/usr/bin/env python
-
 from models import Base, User
-from flask import Flask, jsonify, request, url_for, abort
+from flask import Flask, jsonify, request, url_for, abort, g
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 
-from flask import Flask
+from flask.ext.httpauth import HTTPBasicAuth
+auth = HTTPBasicAuth()
+
 
 engine = create_engine('sqlite:///users.db')
 
@@ -15,30 +15,46 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 app = Flask(__name__)
 
-@app.route('/api/users', methods = ['POST'])
+@auth.verify_password
+def verify_password(username, password):
+    user = session.query(User).filter_by(username = username).first()
+    if not user or not user.verify_password(password):
+        return False
+    g.user = user
+    return True
+
+
+@app.route('/users', methods = ['POST'])
 def new_user():
-    DBSession = sessionmaker(bind=engine)
-    session = DBSession()
     username = request.json.get('username')
     password = request.json.get('password')
     if username is None or password is None:
-        abort(400) # missing arguments
+        print "missing arguments"
+        abort(400) 
+        
     if session.query(User).filter_by(username = username).first() is not None:
-        abort(400) # existing user
+        print "existing user"
+        user = session.query(User).filter_by(username=username).first()
+        return jsonify({'message':'user already exists'}), 200#, {'Location': url_for('get_user', id = user.id, _external = True)}
+        
     user = User(username = username)
     user.hash_password(password)
     session.add(user)
     session.commit()
-    return jsonify({ 'username': user.username }), 201, {'Location': url_for('get_user', id = user.id, _external = True)}
+    return jsonify({ 'username': user.username }), 201#, {'Location': url_for('get_user', id = user.id, _external = True)}
 
 @app.route('/api/users/<int:id>')
 def get_user(id):
-    DBSession = sessionmaker(bind=engine)
-    session = DBSession()
     user = session.query(User).filter_by(id=id).one()
     if not user:
         abort(400)
     return jsonify({'username': user.username})
+
+@app.route('/api/resource')
+@auth.login_required
+def get_resource():
+    return jsonify({ 'data': 'Hello, %s!' % g.user.username })
+
 
 
 if __name__ == '__main__':
