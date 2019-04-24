2c2af8edec103914074dae4e601997becbf9ac51
Perry Brandiezs <pbrandiezs@gmail.com>
2019-04-24 08:48:47 -0700

    feat: Updated views.py with example code.

--- views.py
+++ views.py
@@ -1,82 +1,39 @@
-#!/usr/bin/env python
-
-from findARestaurant import findARestaurant
-from models import Base, Restaurant
-from flask import Flask, jsonify, request
+from models import Base, User
+from flask import Flask, jsonify, request, url_for, abort
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 
-import sys
-import codecs
-sys.stdout = codecs.getwriter('utf8')(sys.stdout)
-sys.stderr = codecs.getwriter('utf8')(sys.stderr)
+from flask import Flask
 
-
-
-
-#foursquare_client_id = ''
-
-#foursquare_client_secret = ''
-
-#google_api_key = ''
-
-engine = create_engine('sqlite:///restaurants.db')
+engine = create_engine('sqlite:///users.db')
 
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 app = Flask(__name__)
 
-@app.route('/restaurants', methods = ['GET', 'POST'])
-def all_restaurants_handler():
-    DBSession = sessionmaker(bind=engine)
-    session = DBSession()
-    #YOUR CODE HERE
-    if request.method == 'GET':
-        # RETURN ALL RESTAURANTS IN DATABASE
-        restaurants = session.query(Restaurant).all()
-        return jsonify(restaurants = [i.serialize for i in restaurants])
-    elif request.method == 'POST':
-        # MAKE A NEW RESTAURANT AND STORE IT IN DATABASE
-        location = request.args.get('location', '')
-        mealType = request.args.get('mealType', '')
-        restaurant_info = findARestaurant(mealType, location)
-        if restaurant_info != "No Restaurants Found":
-            restaurant = Restaurant(restaurant_name = unicode(restaurant_info['name']), restaurant_address = unicode(restaurant_info['address']), restaurant_image = restaurant_info['image'])
-            session.add(restaurant)
-            session.commit() 
-            return jsonify(restaurant = restaurant.serialize)
-        else:
-            return jsonify({"error":"No Restaurants Found for %s in %s" % (mealType, location)})
-    
-@app.route('/restaurants/<int:id>', methods = ['GET','PUT', 'DELETE'])
-def restaurant_handler(id):
-    DBSession = sessionmaker(bind=engine)
-    session = DBSession()
-    #YOUR CODE HERE
-    restaurant = session.query(Restaurant).filter_by(id = id).one()
-    if request.method == 'GET':
-        #RETURN A SPECIFIC RESTAURANT
-        return jsonify(restaurant = restaurant.serialize)
-    elif request.method == 'PUT':
-        #UPDATE A SPECIFIC RESTAURANT
-        address = request.args.get('address')
-        image = request.args.get('image')
-        name = request.args.get('name')
-        if address:
-            restaurant.restaurant_address = address
-        if image:
-            restaurant.restaurant_image = image
-        if name:
-            restaurant.restaurant_name = name
-        session.commit()
-        return jsonify(restaurant = restaurant.serialize)
-    elif request.method == 'DELETE':
-        #DELETE A SPECFIC RESTAURANT
-        session.delete(restaurant)
-        session.commit()
-        return "Restaurant Deleted"
+@app.route('/api/users', methods = ['POST'])
+def new_user():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        abort(400) # missing arguments
+    if session.query(User).filter_by(username = username).first() is not None:
+        abort(400) # existing user
+    user = User(username = username)
+    user.hash_password(password)
+    session.add(user)
+    session.commit()
+    return jsonify({ 'username': user.username }), 201, {'Location': url_for('get_user', id = user.id, _external = True)}
+
+@app.route('/api/users/<int:id>')
+def get_user(id):
+    user = session.query(User).filter_by(id=id).one()
+    if not user:
+        abort(400)
+    return jsonify({'username': user.username})
+
 
 if __name__ == '__main__':
     app.debug = True
