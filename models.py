a9a8f477fafe4422e5b35b59e346b51cebdf40f5
Perry Brandiezs <pbrandiezs@gmail.com>
2019-04-24 08:40:13 -0700

    feat: Updated models.py with example code provided

--- models.py
+++ models.py
@@ -1,36 +1,26 @@
-#!/usr/bin/env python
-
-# -*- coding: utf-8 -*-
-from sqlalchemy import Column,Integer,String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
-
-
-
-
+from passlib.apps import custom_app_context as pwd_context
 
 Base = declarative_base()
-class Restaurant(Base):
-  __tablename__ = 'restaurant'
-  id = Column(Integer, primary_key = True)
-  restaurant_name = Column(String)
-  restaurant_address = Column(String)
-  restaurant_image = Column(String)
-  
-  
-  #Add a property decorator to serialize information from this database
-  @property
-  def serialize(self):
-    return {
-      'restaurant_name': self.restaurant_name,
-      'restaurant_address': self.restaurant_address,
-      'restaurant_image' : self.restaurant_image,
-      'id' : self.id
-      
-      }
 
-engine = create_engine('sqlite:///restaurants.db')
- 
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(32), index=True)
+    password_hash = Column(String(64))
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+
+engine = create_engine('sqlite:///users.db')
 
 Base.metadata.create_all(engine)
